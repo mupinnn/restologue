32377997bc78fd3b487e3251e3bafa2327fa47ca
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-07 22:19:50 +0700

    feat(navbar): drawer toggler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,16 @@ import "./home.js";
 if (module.hot) {
   module.hot.accept();
 }
+
+const navToggle = document.querySelector(".navbar-toggle");
+const navMenu = document.querySelector(".navbar-menu");
+
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+
+  if (navMenu.classList.contains("open")) {
+    navToggle.textContent = "Close Menu";
+  } else {
+    navToggle.textContent = "Open Menu";
+  }
+});
