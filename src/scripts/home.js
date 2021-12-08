cf0186fb41ced9857d31ff8f50bd8cd29ee16eac
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-08 15:07:50 +0700

    feat(components): footer

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,6 +1,9 @@
+const footerYear = document.getElementById("year");
+
 window.addEventListener("load", () => {
   // Prevent transition jump/flash by removing transition using .preload class
   // that applied to body and remove the class after page fully loaded to bring
   // back the transition ability to all element.
   document.body.classList.remove("preload");
+  footerYear.innerText = new Date().getFullYear();
 });
