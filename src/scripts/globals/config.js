5685e067b51b68d91da50f1ac3c1abf0fa6d87fe
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-21 22:04:15 +0700

    feat: global config

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  API_BASE_URL: "https://restaurant-api.dicoding.dev/",
+  API_IMG_BASE_URL: "https://restaurant-api.dicoding.dev/images/",
+  CACHE_NAME: "restologue-v1",
+  DB_NAME: "restologue",
+  DB_VERSION: 1,
+  OBJECT_STORE_NAME: "fav-resto",
+};
+
+export default CONFIG;
