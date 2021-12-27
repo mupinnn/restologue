ae5922427bbb12d9bddf65193e4e247fbd8dc69c
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-27 14:24:54 +0700

    feat(favorites): request status indicator

--- src/scripts/views/pages/favorites.js
+++ src/scripts/views/pages/favorites.js
@@ -1,4 +1,8 @@
-import { createRestoItemTemplate } from "~/views/templates/template-creator";
+import {
+  createRestoItemTemplate,
+  createLoadingTemplate,
+  createErrorTemplate,
+} from "~/views/templates/template-creator";
 import FavRestoIdb from "~/data/favresto-idb";
 
 const Favorites = {
@@ -7,17 +11,31 @@ const Favorites = {
       <section class="container main-section">
         <h2>Your Favorite Restos</h2>
 
+        <div class="request-status"></div>
+
         <article class="resto-list"></article>
       </section>
     `;
   },
 
   async afterRender() {
-    const restos = await FavRestoIdb.getAllResto();
     const restosContainer = document.querySelector(".resto-list");
-    restos.forEach((resto) => {
-      restosContainer.innerHTML += createRestoItemTemplate(resto);
-    });
+    const reqStatusContainer = document.querySelector(".request-status");
+    reqStatusContainer.innerHTML = createLoadingTemplate();
+
+    try {
+      const restos = await FavRestoIdb.getAllResto();
+
+      if (restos.length === 0) {
+        reqStatusContainer.innerHTML = createErrorTemplate();
+      }
+
+      restos.forEach((resto) => {
+        restosContainer.innerHTML += createRestoItemTemplate(resto);
+      });
+    } catch (e) {
+      reqStatusContainer.innerHTML = createErrorTemplate();
+    }
   },
 };
 
