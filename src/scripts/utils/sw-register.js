c7aed6c1acfa5ac37f0643da4312342742196898
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-27 13:13:40 +0700

    feat(sw): move not supported log to else block

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -3,9 +3,9 @@ import runtime from "serviceworker-webpack-plugin/lib/runtime";
 const swRegister = async () => {
   if ("serviceWorker" in navigator) {
     await runtime.register();
+  } else {
+    console.log("Service Worker not supported in this browser.");
   }
-
-  console.log("Service Worker not supported in this browser.");
 };
 
 export default swRegister;
