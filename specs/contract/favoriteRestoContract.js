cbf40d31ea77ab6de882597d61123a1c30eba006
Ahmad Muwaffaq <itsmupin@gmail.com>
2021-12-31 23:42:37 +0700

    test: `FavRestoIdb` with contract test
    test(`FavRestoIdb`): should retun the resto that has been added passed
    
    test(`FavRestoIdb`): should refuse if does not have correct props failing
    
    test(`FavRestoIdb`): should refuse if does not have correct props passed
    
    test(`FavRestoIdb`): can return all favorited resto failing
    
    test(`FavRestoIdb`): can return all favorited resto passed
    
    test(`FavRestoIdb`): should remove favorite resto failing
    
    test(`FavRestoIdb`): should remove favorite resto passed
    
    test(`FavRestoIdb`): should handle remove favorite resto even has not been added failing
    
    test(`FavRestoIdb`): should handle remove favorite resto even has not been added passed

--- specs/contract/favoriteRestoContract.js
+++ specs/contract/favoriteRestoContract.js
@@ -1,10 +1,48 @@
 const itActAsFavoriteRestoModel = (favoriteResto) => {
   it("should return the resto that has been added", async () => {
-    favoriteResto.putResto({ id: 1 });
+    await favoriteResto.putResto({ id: 1 });
+    await favoriteResto.putResto({ id: 2 });
 
-    expect(favoriteResto.getResto(1)).toEqual({ id: 1 });
-    expect(favoriteResto.getResto(2)).toEqual({ id: 2 });
-    expect(favoriteResto.getResto(3)).toEqual(undefined);
+    expect(await favoriteResto.getResto(1)).toEqual({ id: 1 });
+    expect(await favoriteResto.getResto(2)).toEqual({ id: 2 });
+    expect(await favoriteResto.getResto(3)).toEqual(undefined);
+  });
+
+  it("should refuse a resto from being added if it does not have correct property", async () => {
+    await favoriteResto.putResto({ wrongProps: "Oops!" });
+
+    expect(await favoriteResto.getAllResto()).toEqual([]);
+  });
+
+  it("can return all the restos that have been added", async () => {
+    await favoriteResto.putResto({ id: 1 });
+    await favoriteResto.putResto({ id: 2 });
+
+    expect(await favoriteResto.getAllResto()).toEqual([{ id: 1 }, { id: 2 }]);
+  });
+
+  it("should remove favorite resto", async () => {
+    await favoriteResto.putResto({ id: 1 });
+    await favoriteResto.putResto({ id: 2 });
+    await favoriteResto.putResto({ id: 3 });
+
+    await favoriteResto.deleteResto(2);
+
+    expect(await favoriteResto.getAllResto()).toEqual([{ id: 1 }, { id: 3 }]);
+  });
+
+  it("should handle request to remove a resto event though the resto has not been added", async () => {
+    await favoriteResto.putResto({ id: 1 });
+    await favoriteResto.putResto({ id: 2 });
+    await favoriteResto.putResto({ id: 3 });
+
+    await favoriteResto.deleteResto(4);
+
+    expect(await favoriteResto.getAllResto()).toEqual([
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+    ]);
   });
 };
 
