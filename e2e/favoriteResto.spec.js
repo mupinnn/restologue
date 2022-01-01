edd4ae988757e28ecce12406292d958ea8e63cad
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-01-01 12:23:30 +0700

    test(e2e): favoriting one restaurant passed

--- e2e/favoriteResto.spec.js
+++ e2e/favoriteResto.spec.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+
 Feature("Favorite a resto");
 
 Before((I) => {
@@ -6,4 +8,25 @@ Before((I) => {
 
 Scenario("showing empty favorite restos", (I) => {
   I.see("Unfortunately, the content is empty.", ".request-status p");
+});
+
+Scenario("favoriting one restaurant", async (I) => {
+  I.see("Unfortunately, the content is empty.", ".request-status p");
+
+  I.amOnPage("/");
+  I.seeElement("a .resto-list-item h3");
+
+  const firstResto = locate("a .resto-list-item h3").first();
+  const firstRestoName = await I.grabTextFrom(firstResto);
+  I.click(firstResto);
+
+  I.seeElement("#favButton");
+  I.click("#favButton");
+
+  I.amOnPage("/#/favorites");
+  I.see("Your Favorite Restos", ".main-section > h2");
+  I.seeElement(".resto-list-item h3");
+
+  const favedRestoName = await I.grabTextFrom(".resto-list-item h3");
+  assert.strictEqual(firstRestoName, favedRestoName);
 });
