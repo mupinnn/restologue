eb875eeec2a6f0b29a5b7ec23ffe35b7283657a1
Ahmad Muwaffaq <itsmupin@gmail.com>
2022-01-01 14:09:17 +0700

    chore: compress `.jp(e)g`

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,34 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const ImageminWebpackPlugin = require("imagemin-webpack-plugin").default;
+const ImageminMozJpeg = require("imagemin-mozjpeg");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: '/node_modules/',
+        exclude: "/node_modules/",
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
             },
           },
         ],
       },
     ],
   },
+  plugins: [
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozJpeg({
+          quality: 50,
+          progressive: true,
+        }),
+      ],
+    }),
+  ],
 });
